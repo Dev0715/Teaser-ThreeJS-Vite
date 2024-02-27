57495a70aa9e8054935c291bd351e0c5dc6d65b9
Dev0715 <webdev0715@gmail.com>
2024-02-27 16:00:18 +0500

    Changed the vite config to make the server available in the local network (can now open local server on the phone)

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,9 @@ export default defineConfig({
     emptyOutDir: true,
     sourcemap: false,
   },
+  server:
+  {
+      host: true, // Open to local network and display URL
+      open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+  },
 });
