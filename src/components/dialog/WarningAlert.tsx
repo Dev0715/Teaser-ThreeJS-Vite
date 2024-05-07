8346fc50f851d55d40539901ff2bd1af0c52a0fc
Dev0715 <webdev0715@gmail.com>
2024-05-07 12:28:52 +0900

    update: warning alert

--- src/components/dialog/WarningAlert.tsx
+++ src/components/dialog/WarningAlert.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
-const WarningAlert = ({ message }: { message: string }) => {
+const WarningAlert = ({
+  message,
+  setMessage,
+}: {
+  message: string;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const onOK = () => {
+    setMessage('');
+  };
+
   return (
     <div
       className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-700/50 z-50 rounded-lg"
       role="alert"
     >
-      <div className="w-96 flex flex-col bg-white">
-        <p className="font-bold">Warning</p>
+      <div className="w-96 flex flex-col gap-y-2 px-6 py-4 bg-white rounded-xl">
+        <p className="text-xl font-bold">Warning</p>
         <p>{message}</p>
+        <button className="hover:drop-shadow" onClick={onOK}>
+          OK
+        </button>
       </div>
     </div>
   );
