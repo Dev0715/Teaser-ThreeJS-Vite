8cc8c37071f0ed44c885e97ba332ea9459ac3abf
Dev0715 <webdev0715@gmail.com>
2024-05-06 21:27:52 -0400

    fix: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Leva } from 'leva';
 
 import Layout from './components/Layout';
+import { useAudiate } from './lib/use-audiate';
 
 export default function App() {
+  const [audioContextState, isEnabled] = useAudiate();
   const [play, setPlay] = useState(false); // start music
   const [debugValue, setDebug] = useState(true); // show debug panel
+
+  console.log(audioContextState, isEnabled);
 
   const parentCallback = (play: boolean) => {
     setPlay(!play);
