4cfda0b7bb5962fcfbadc128e028faf17d4173a2
Dev0715 <webdev0715@gmail.com>
2024-05-07 03:16:24 -0400

    feat: add preloader

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import { Leva } from 'leva';
 import Layout from './components/Layout';
 import { useAudiate } from './hooks/use-audiate';
 
+export const DELAY_TIME = 3000;
+
 export default function App() {
   const [audioContextState, isEnabled] = useAudiate();
   const [play, setPlay] = useState(false); // start music
   const [debugValue, setDebug] = useState(true); // show debug panel
 
   console.log(audioContextState, isEnabled);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const loader = document.getElementById('loader');
+      loader?.classList.add('hide');
+    }, DELAY_TIME);
+  }, []);
 
   const handlePlay = (play: boolean) => {
     setPlay(play);
