05f9ff7aaa1f8da008be06351d370fb57c04da13
Dev0715 <webdev0715@gmail.com>
2024-05-08 07:36:01 +0900

    update: style of warning alert

--- src/components/dialog/WarningAlert.tsx
+++ src/components/dialog/WarningAlert.tsx
@@ -13,12 +13,14 @@ const WarningAlert = ({
 
   return (
     <div
-      className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-700/50 z-50 rounded-lg"
+      className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-700/50 z-50 rounded-lg"
       role="alert"
     >
       <div className="w-96 flex flex-col gap-y-4 p-6 bg-white rounded-xl">
-        <p className="text-xl text-black font-bold">Warning</p>
-        <p>{message}</p>
+        <div className="flex flex-col gap-y-2">
+          <p className="text-xl text-black font-bold">Error</p>
+          <p>{message}</p>
+        </div>
         <button
           className="w-fit flex self-center text-black border px-4 rounded-md hover:drop-shadow"
           onClick={onOK}
