089cd0ac3f82f592c1f654e4392f728984368fe5
Dev0715 <webdev0715@gmail.com>
2024-05-08 22:29:39 -0400

    fix: position marker content

--- src/components/PositionMarker.tsx
+++ src/components/PositionMarker.tsx
@@ -14,14 +14,28 @@ function PositionMarker() {
     <>
       {/* Left */}
       <div className="left-node absolute flex flex-row left-10 top-[48%] z-50">
-        <div className="left-node-svg w-[87px] h-[65px] bg-no-repeat bg-center pt-5 mr-5 bg-[url('../img/LeftNode.png')]"></div>
-        <div className="left-node-text">{posX}</div>
+        <div className="left-node-svg w-[87px] h-[65px] bg-no-repeat bg-center mr-5 bg-[url('../img/LeftNode.png')]"></div>
+        <div className="left-node-text text-xs">
+          X:
+          <br />
+          {posX}
+          <br />
+          <br />
+          /VOID
+        </div>
       </div>
 
       {/* Right */}
       <div className="right-node absolute flex flex-row right-10 top-[48%] z-50">
-        <div className="right-node-text">{posY}</div>
-        <div className="right-node-svg w-[93px] h-[65px] bg-no-repeat bg-center pt-5 mr-5 bg-[url('../img/RightNode.png')]"></div>
+        <div className="right-node-text text-xs text-right">
+          Y:
+          <br />
+          {posY}
+          <br />
+          <br />
+          /VOID
+        </div>
+        <div className="right-node-svg w-[93px] h-[65px] bg-no-repeat bg-center ml-5 bg-[url('../img/RightNode.png')]"></div>
       </div>
     </>
   );
