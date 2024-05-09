65f339ecd0b3151ce89203ca15c7a1d844aad45f
Dev0715 <webdev0715@gmail.com>
2024-05-09 10:33:08 +0900

    rearrange: update alert dialog

--- src/components/dialog/AlertDialog.tsx
+++ src/components/dialog/AlertDialog.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const WarningAlert = ({
+  title,
   message,
   setMessage,
 }: {
+  title: string;
   message: string;
   setMessage: React.Dispatch<React.SetStateAction<string>>;
 }) => {
@@ -18,7 +20,7 @@ const WarningAlert = ({
     >
       <div className="w-96 flex flex-col gap-y-4 p-6 bg-white rounded-xl">
         <div className="flex flex-col gap-y-2">
-          <p className="text-xl text-black font-bold">Error</p>
+          <p className="text-xl text-black font-bold">{title}</p>
           <p>{message}</p>
         </div>
         <button
