adab134cdcaa2a1f4492032e940469374c7bf586
Dev0715 <webdev0715@gmail.com>
2024-05-10 15:57:36 +0900

    update: unsubscription dialog

--- src/components/dialog/UnSubscriptionDialog.tsx
+++ src/components/dialog/UnSubscriptionDialog.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { DialogCancelParam } from '../Subscription';
+import OtpInput, { NUMBER_OF_OTP_DIGIT } from '../OtpInput';
 
 const STEP_UNSUBSCRIBE = 'step_unsubscribe';
 const STEP_OTP = 'step_otp';
@@ -26,8 +27,6 @@ const BUTTON_TEXT: Record<string, string>[] = [
   },
 ];
 
-const NUMBER_OF_OTP_DIGIT = 6;
-
 type UnsubscribeProps = {
   email: string;
   onCancel: (params: DialogCancelParam) => void;
@@ -41,33 +40,11 @@ const UnsubscribeDialog = ({ email, onCancel }: UnsubscribeProps) => {
   const [otp, setOtp] = useState<string[]>(
     new Array(NUMBER_OF_OTP_DIGIT).fill(''),
   );
-  const optRef = useRef(new Array(NUMBER_OF_OTP_DIGIT).fill(HTMLInputElement));
 
-  function handleChange(value: string, index: number) {
-    let newArr = [...otp];
-    newArr[index] = value;
-    setOtp(newArr);
-
-    if (value && index < NUMBER_OF_OTP_DIGIT - 1) {
-      optRef.current[index + 1].focus();
-    }
-  }
-
-  function handleBackspaceAndEnter(
-    e: React.KeyboardEvent<HTMLInputElement>,
-    index: number,
-  ) {
-    if (e.key === 'Backspace' && !e.currentTarget.value && index > 0) {
-      optRef.current[index - 1].focus();
-    }
-    if (
-      e.key === 'Enter' &&
-      e.currentTarget.value &&
-      index < NUMBER_OF_OTP_DIGIT - 1
-    ) {
-      optRef.current[index + 1].focus();
-    }
-  }
+  const onOtpChanged = (otp: string[]) => {
+    setOtp(otp);
+    setErrMsg('');
+  };
 
   const unsubscribe = () => {
     const base_url = import.meta.env.VITE_MAILMANJS_API_URL as string;
@@ -148,19 +125,7 @@ const UnsubscribeDialog = ({ email, onCancel }: UnsubscribeProps) => {
         </div>
 
         {step === STEP_OTP && (
-          <div className="flex items-center gap-x-4">
-            {otp.map((digit, index) => (
-              <input
-                key={index}
-                value={digit}
-                maxLength={1}
-                className={`border w-12 h-auto text-black p-3 rounded-md block bg-white focus:border-2 focus:outline-none appearance-none`}
-                onChange={(e) => handleChange(e.target.value, index)}
-                onKeyUp={(e) => handleBackspaceAndEnter(e, index)}
-                ref={(reference) => (optRef.current[index] = reference)}
-              />
-            ))}
-          </div>
+          <OtpInput otp={otp} valueChanged={onOtpChanged} />
         )}
 
         {errMsg && <p className="text-red-500 text-sm">{errMsg}</p>}
